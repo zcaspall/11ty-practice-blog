0e529541d991eb56c33a9352e650df5ef4c2919d
Zach Caspall <zcaspall@hotmail.com>
2021-06-25 00:59:00 -0500

    11tyfied the static pages

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+const { DateTime } = require("luxon");
+
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy('./src/style.css');
+    eleventyConfig.addPassthroughCopy('./src/assets');
+    
+    eleventyConfig.addFilter("postDate", (dataObj) => {
+        return DateTime.fromJSDate(dataObj).toLocaleString(DateTime.DATE_MED);
+    });
+
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    };
+}
